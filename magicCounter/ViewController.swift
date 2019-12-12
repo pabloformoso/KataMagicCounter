b5acf99cd3e34b02d510bd9b15c3f886ff453746
Pablo Formoso Estrada <pablo@pabloformoso.com>
2019-12-12 11:40:52 +0100

    Initial Commit

---
+++ magicCounter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  magicCounter
+//
+//  Created by Pablo Formoso Estrada on 12/12/2019.
+//  Copyright © 2019 FormoSil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
